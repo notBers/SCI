5769ec31cb72a2715a9ebe24211ae027cd3b38d5
notBers <109546794+notBers@users.noreply.github.com>
2024-03-24 12:10:16 -0600

    Update Hpcomponents.js

--- src/components/subcomponents/Hpcomponents.js
+++ src/components/subcomponents/Hpcomponents.js
@@ -42,7 +42,7 @@ export function FSection(){
             </div>
             <div className="countdown-box">
                 <div id="countdown">
-                        <span>Evento en progreso!!</span>
+                        <span>Evento finalizado</span>
                 </div>
             </div>
         </div>
